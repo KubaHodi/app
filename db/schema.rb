c8cda5879df53f2d221a2cfffd759555f9307d3d
Kuba Hodi <jakhod2007@gmail.com>
2024-12-12 21:04:32 +0100

    Added categories to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_12_102619) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_12_195855) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -19,5 +25,9 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_12_102619) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
+
+  add_foreign_key "products", "categories"
 end
