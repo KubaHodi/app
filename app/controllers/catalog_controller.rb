15b73d3790eedef4e830029809c71575399842a9
KubaHodi <jakhod2007@gmail.com>
2024-12-15 14:59:11 +0100

    Working on listing categories

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,6 +1,6 @@
 class CatalogController < ApplicationController
   def index
-    @products = Product.order(:category_id)
+    @products = Product
     @categories = Category.find_by_id(params[:category_id])
   end
 end
