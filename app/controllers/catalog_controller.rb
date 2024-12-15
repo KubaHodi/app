a86dc96ae38eb9e646f8adf62aa64a0a416c5688
Kuba Hodi <jakhod2007@gmail.com>
2024-12-15 15:47:59 +0100

    Added first product photo and image displaying + editing catalog index to display selectable genres from categories

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,6 +1,6 @@
 class CatalogController < ApplicationController
   def index
-    @products = Product
+    @products = Product.all
     @categories = Category.find_by_id(params[:category_id])
   end
 end
