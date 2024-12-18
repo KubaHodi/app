08d6f851382b61fdbe8dcbb93de73ede64e0ca3d
KubaHodi <jakhod2007@gmail.com>
2024-12-18 22:00:14 +0100

    Quick fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to admin_url
     else
-      redirect_to login_url, alert: "Invalid user or password"
+      redirect_to login_url, notice: "Invalid user or password"
     end
   end
 
