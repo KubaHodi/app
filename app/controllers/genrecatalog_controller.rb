f4cab8cdbae02006fe1962b6871b4b5cf3e3bb90
KubaHodi <jakhod2007@gmail.com>
2024-12-18 10:58:54 +0100

    Generated controller about us and modified products explorer

--- app/controllers/genrecatalog_controller.rb
+++ app/controllers/genrecatalog_controller.rb
@@ -1,9 +1,12 @@
 class GenrecatalogController < ApplicationController
   def index
     current_genre_id = Genre.find_by_id(params[:genre_id])
+    current_category_id = $cat_id
+
     @genres = current_genre_id
+    @categories = current_category_id
     @products = Product.where(category_id: $cat_id, genre_id: current_genre_id)
-    @categories = Category.all
+    
   end
 
   
