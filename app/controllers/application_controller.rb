5853329f77bb2c180769ca08a26f4ae2724b7485
Kuba Hodi <jakhod2007@gmail.com>
2024-12-19 21:11:15 +0100

    Added current_user method in order to get current user id that is in the session, reworked notices and forms, added better styling for admin view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authorize
 
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+  
   protected
   def authorize
     unless User.find_by(id: session[:user_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize
+  after_action :find_user
   def new
   end
 
@@ -7,14 +8,19 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      redirect_to admin_url, notice: "Logged In As #{user.name}"
     else
-      redirect_to login_url, notice: "Invalid user or password"
+      redirect_to login_url, alert: "Invalid user or password"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out"
+    redirect_to root_url, notice: "Successfully Logged Out"
+  end
+
+  private 
+  def find_user
+    $user_id_for_display = session[:user_id]
   end
 end
