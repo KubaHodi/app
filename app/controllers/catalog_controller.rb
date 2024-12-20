3f03e11af0bf1331f192521aed87f3a2f71e0315
KubaHodi <jakhod2007@gmail.com>
2024-12-20 12:59:17 +0100

    Quick fixes in controllers

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,7 +2,7 @@ class CatalogController < ApplicationController
   skip_before_action :authorize
   def index
     @products = Product.all
-    @categories = Category.find_by_id(params[:category_id])
+    @category = Category.find_by_id(params[:category_id])
     @genres = Genre.all
     $cat_id = Category.find_by_id(params[:category_id]) #global variable for storing category_id all over controllers
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authorize
   def index
-    @products = Product.order(:category_id)
     @categories = Category.order(:name)
   end
 end
