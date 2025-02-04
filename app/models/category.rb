e8808b7ac1c3143dd399b8c93c7852af15548a71
KubaHodi <jakhod2007@gmail.com>
2025-02-04 23:25:33 +0100

    Fixing test in the application

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,13 @@ class Category < ApplicationRecord
   has_one_attached :category_image
   has_many :products
   validates :name, presence: true, uniqueness: true
+  validates :category_image, presence: true
+  validate :correct_image_mime_type
+  private
+  def correct_image_mime_type
+    if category_image.attached? && !category_image.content_type.in?(%w(image/png))
+      category_image.purge
+      errors.add(:image_url, 'Must be a PNG file')
+    end
+  end
 end
